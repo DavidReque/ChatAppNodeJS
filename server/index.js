df8cfff6550fc6b87ce14744ac24b520b4c166b5
David Requeno <davidrequeno52@gmail.com>
2024-03-08 20:00:23 -0600

    eslint

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-import express from "express";
-import logger from "morgan";
+import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
-const port = process.env.PORT ?? 3000;
-const app = express();
+const app = express()
+const port = process.env.PORT ?? 3000
+const server = createServer(app)
+const io = new Server(server)
 
-app.use(logger("dev"));
+io.on('connection', () => {
+  console.log('a user has connected')
+})
 
-app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/client/index.html");
-});
+app.use(logger('dev'))
 
-app.listen(port, () => {
-  console.log("Server runnig");
-});
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
+  console.log('Server runnig')
+})
