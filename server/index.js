31ec3273e185df06d6a758c05e5957b6f6c8aafb
David Requeno <davidrequeno52@gmail.com>
2024-03-09 20:39:24 -0600

    event

--- server/index.js
+++ server/index.js
@@ -8,8 +8,16 @@ const port = process.env.PORT ?? 3000
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('a user has connected')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message ' + msg)
+  })
 })
 
 app.use(logger('dev'))
